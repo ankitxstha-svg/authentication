6352c24605ad45167b595245d034f3ee10a3b895
Ankit Shrestha <ankitshrestha@Ankits-MacBook-Air.local>
2025-11-17 10:52:35 +1100

    Improve server-side registration logic and add bcrypt password hashing

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import {query} from "./db/db.js"
+import bcrypt from 'bcrypt';
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
 
 const app = express();
 const port = 3000;
+const saltRounds = 2;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+//creating session
+app.use(session({
+  secret:"TOPSECRET",
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+//read and write to session
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -25,13 +42,26 @@ app.post("/register", async (req, res) => {
   const password = req.body.password;
 
   try {
-    const response = await query("INSERT INTO users (username, password) VALUES ($1, $2);",[username, password]);
-  
+    const response = await query("SELECT * FROM users where username = ($1);", [username]);
+
     if(response.rowCount > 0){
-      res.status(200).render("secrets.ejs");
+      res.send("email already exist");
     }else{
-      res.status(500).send("internal server error");
-    }
+
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+          if(err){
+            console.error("error registering: ", err);
+          }else{
+            const response = await query("INSERT INTO users (username, password) VALUES ($1, $2);",[username, hash]);
+
+            if(response.rowCount > 0){
+              res.status(200).render("secrets.ejs");
+            }else{
+              res.status(500).send("internal server error");
+          }}
+        });
+      }
+  
   } catch (error) {
     console.error("error creating user: ", error);
     res.status(500).send("internal server error");
@@ -40,30 +70,46 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
 
-  try {
-    const username = req.body.username;
-    const password = req.body.password;
+});
 
+passport.use(new Strategy(async function verify(username, password, cb){
+  
+  try {
     const response = await query("select * from users where username = ($1);", [username]);
 
     if(response.rowCount > 0){
       const checkPassword = response.rows[0].password;
+      const user = response.rows[0];
 
-      if(checkPassword === password){
-        res.status(200).send("matched password");
-      }else{
-        res.status(401).send("invalid credentials");
-      }
+      bcrypt.compare(password, checkPassword, (err, result) => {
+        if(err){
+          return cb(err);
+        }else{
+          if(result){
+            return cb(null, user);
+          }else{
+            return cb(null, false);
+          }
+        }
+      })
     }else{
-      res.status(404).send("no user found");
+      return cb("user not found");
     }
 
   } catch (error) {
-    
+    return cb(error); 
   }
+}));
 
-
+app.get("/secrets", (req, res)=>{
+  if(req.isAuthenticated()){
+    res.render("secrets.ejs")
+  }else{
+    res.redirect("/login");
+  }
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
